f31bd8006de8d3e5ff9e48e344baf1bee44279cc
Kirat <kanwaldeep.puri@gmail.com>
2023-07-31 16:38:16 +0530

    updates to requirements.txt file

--- sms.py
+++ sms.py
@@ -10,8 +10,8 @@ import smtplib
 from string import Template
 from email.message import EmailMessage
 from io import BytesIO
+from flask import Flask, render_template, request, redirect
 # from pathlib import path
-
 
 # directory = sys.argv[1]
 st.title("M_Ale Dashboard")
@@ -22,6 +22,11 @@ bod = st.text_area("Please draft a body")
 uploaded_file = st.file_uploader("Drag and Drop your mail recipient list here as csv.", type=["csv"], accept_multiple_files=False, label_visibility="visible")
 but = st.button("Send")
 html = Template(Path("index.html").read_text())
+
+@app.route('/image_link')
+def hello_img():
+    print("Hurrrrrrreeeeeeeeeeeeee!!!!!!!!!!!!!!!!!!!!!!!!")
+    return render_template("https://storage.googleapis.com/male-8b6f5.appspot.com/Tracking_Pixel/kirat/1.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=firebase-adminsdk-qza06%40male-8b6f5.iam.gserviceaccount.com%2F20230731%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230731T100224Z&X-Goog-Expires=3600&X-Goog-SignedHeaders=host&X-Goog-Signature=74b03758b85443e1e51f83239556e3e19e808b62d35074f87e9cf61dc0431839bcda6b61ca554ff3b13efe50601a43a3d7cc2cb01945bb26994c4a956cb4cf9715b11757b4b1ab4f6b6e68aa7d4277ea48a8b84a46c4560853300e9eded149a2d00249ec09de686d1096833a9c25ad3a7792af0b9aeffa0f7faf886fb8a690e3e372dde9c3d5e598a679001b378177d28818f0ecc37d6c62b6e321fec244615c8e95d3a4b225fadb38b9980dc2a67ef1e30a369f4756b3986c6be9f7829c30c8b1dcba39cc5501f12070c8efe908239a74ba7449d1046d8aad73614c119204f77f496b7ee0a52dce17404ee733f19e35a9b967f7ce31135a7f13abc9e002316c")
 
 
 def upload_image_to_firebase_storage(image_object, destination_path):
@@ -53,7 +58,6 @@ def get_shareable_link(image_path):
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
-
 
 
 if uploaded_file is not None and but == True and sub != "":
